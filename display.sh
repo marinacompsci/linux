2ff30d24aa34d2f28b4f4d4ea30ed72ef0343286
MBSA <marinacompsci@protonmail.com>
2025-07-13 22:35:59 +0200

    fix: add variables

--- display.sh
+++ display.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-xrandr --newmode  2>&1 | logger -t i3-xrandr
-sleep 1 && xrandr --addmode Virtual-1  2>&1 | logger -t i3-xrandr
-sleep 1 && xrandr --output Virtual-1 --mode  2>&1 | logger -t i3-xrandr
+xrandr --newmode "1712x1112_60.00" 158.25 1712 1824 2000 2288 1112 1115 1125 1154 -hsync +vsync 2>&1 | logger -t i3-xrandr
+sleep 2 && xrandr --addmode Virtual-1 1712x1112_60.00 2>&1 | logger -t i3-xrandr
+sleep 2 && xrandr --output Virtual-1 --mode 1712x1112_60.00 2>&1 | logger -t i3-xrandr
