ac9bb5fb4eca452cd5a50c45dfeff48828586265
Marina Santana <marinacompsci@protonmail.com>
2025-12-13 18:37:25 +0100

    refactor: use pnpm instead of npm

--- post-install.sh
+++ post-install.sh
@@ -2,11 +2,10 @@
 
 echo 'Run me on a TMUX split window and not i3.'
 echo 'Install NPM packages.'
-nvm install node
-npm i -g bash-language-server
-npm i -g pyright
-npm i -g typescript-language-server typescript
-npm i -g vscode-langservers-extracted
+curl -fsSL https://get.pnpm.io/install.sh | sh -
+pnpm i -g bash-language-server
+pnpm i -g pyright
+pnpm i -g vscode-langservers-extracted
 
 echo 'Install Go language server.'
 go install golang.org/x/tools/gopls@latest
